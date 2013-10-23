2fb6e24b60ac5a8eac672d787acedbcc78a1ad9f
Marco Carvalho <marco.carvalho.swasthya@gmail.com>
2013-10-22 22:40:08 -0200

    Fixing namespaces

--- app/models/spree/calculator/pac.rb
+++ app/models/spree/calculator/pac.rb
@@ -1,22 +1,24 @@
 module Spree
-  class Calculator::PAC < Calculator::CorreiosBaseCalculator
-    def self.description
-      "PAC"
-    end
-    
-    def shipping_method
-      if has_contract?
-        :pac_com_contrato
-      else
-        :pac
+  module Calculator::Shipping
+    class PAC < CorreiosBaseCalculator
+      def self.description
+        "PAC"
       end
-    end
-    
-    def shipping_code
-      if has_contract?
-        41068
-      else
-        41106
+      
+      def shipping_method
+        if has_contract?
+          :pac_com_contrato
+        else
+          :pac
+        end
+      end
+      
+      def shipping_code
+        if has_contract?
+          41068
+        else
+          41106
+        end
       end
     end
   end
--- app/models/spree/calculator/sedex.rb
+++ app/models/spree/calculator/sedex.rb
@@ -1,22 +1,24 @@
 module Spree
-  class Calculator::SEDEX < Calculator::CorreiosBaseCalculator
-    def self.description
-      "SEDEX"
-    end
-    
-    def shipping_method
-      if preferred_token.present? && preferred_password.present?
-        :sedex_com_contrato_1
-      else
-        :sedex
+  module Calculator::Shipping
+    class SEDEX < CorreiosBaseCalculator
+      def self.description
+        "SEDEX"
       end
-    end
-    
-    def shipping_code
-      if has_contract?
-        40096
-      else
-        40010
+      
+      def shipping_method
+        if preferred_token.present? && preferred_password.present?
+          :sedex_com_contrato_1
+        else
+          :sedex
+        end
+      end
+      
+      def shipping_code
+        if has_contract?
+          40096
+        else
+          40010
+        end
       end
     end
   end
