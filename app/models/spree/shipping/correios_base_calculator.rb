52d366130e10aa5a4ce05adad6a892816dfbd9a8
Marco Carvalho <marco.carvalho.swasthya@gmail.com>
2013-10-23 21:40:23 -0200

    back prefered fields

--- app/models/spree/shipping/correios_base_calculator.rb
+++ app/models/spree/shipping/correios_base_calculator.rb
@@ -1,11 +1,11 @@
 module Spree
   module Calculator::Shipping
     class CorreiosBaseCalculator < Spree::ShippingCalculator
-      # preference :token, :string
-      # preference :password, :string
-      # preference :declared_value, :boolean, default: false
-      # preference :receipt_notification, :boolean, default: false
-      # preference :receive_in_hands, :boolean, default: false
+      preference :token, :string
+      preference :password, :string
+      preference :declared_value, :boolean, default: false
+      preference :receipt_notification, :boolean, default: false
+      preference :receive_in_hands, :boolean, default: false
       
       attr_reader :delivery_time
       
@@ -30,14 +30,14 @@ module Spree
             c.cep_origem = package.stock_location.zipcode
             c.cep_destino = order.ship_address.zipcode
             c.encomenda = pack
-            # c.valor_declarado = order.amount.to_f if prefers?(:declared_value)
-            # c.mao_propria = prefers?(:receive_in_hands)
-            # c.aviso_recebimento = prefers?(:receipt_notification)
-            # c.codigo_empresa = preferred_token if preferred_token.present?
-            # c.senha = preferred_password if preferred_password.present?
+            c.valor_declarado = order.amount.to_f if prefers?(:declared_value)
+            c.mao_propria = prefers?(:receive_in_hands)
+            c.aviso_recebimento = prefers?(:receipt_notification)
+            c.codigo_empresa = preferred_token if preferred_token.present?
+            c.senha = preferred_password if preferred_password.present?
           end
 
-          webservice = calculator.calculate(:shipping_method)
+          webservice = calculator.calculate(shipping_method)
           return 0.0 if webservice.erro?
           @delivery_time = webservice.prazo_entrega
           webservice.valor
@@ -48,7 +48,7 @@ module Spree
       end
       
       def has_contract?
-        false # preferred_token.present? && preferred_password.present?
+        preferred_token.present? && preferred_password.present?
       end
     end
   end
